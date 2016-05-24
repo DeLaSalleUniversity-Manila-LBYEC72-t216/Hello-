323be96312f3befad1c5c5edaaf7a0b585033296
fionapangilinan <fiona_pangilinan@dlsu.edu.ph>
2016-05-24 16:37:28 +0800

    Add files via upload

---
+++ hellomain.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+	printf("\t @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+	printf("\n\n \t \t Fiona Marian L. Pangilinan");
+	printf("\n\n \t \t \t BS CHE");
+	printf("\n\n \t \t        11410101");
+	printf("\n\n \t \t      May 24, 2016");
+	printf("\n\n \t \t jelliclecats@dlsu.edu.ph");
+	printf("\n\n \t \t       09101010101");
+	printf("\n\n\t @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+
+	return 0;
+	
+}
